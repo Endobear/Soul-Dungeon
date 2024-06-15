e4da337e5f6400de72ccb011523c6b132e238566
support <enriquemessias.souza@gmail.com>
2024-06-15 19:39:55 -0300

    coiso

--- Cenas/Inimizade.gd
+++ Cenas/Inimizade.gd
@@ -7,6 +7,8 @@ var velocidade := 55
 var dano := 1
 var animacoes : SpriteFrames
 var player  : Player  = null
+var estado  = "parado"
+var direcao = "baixo"
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var knockout = $Knockout
@@ -26,27 +28,41 @@ func _ready():
  
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	animated_sprite_2d.play()
+	
 	var distance_from_player
 	var direction
 	if vida <=0:
 		queue_free()
-
-
+	
+	
 	if player:
 		distance_from_player = Vector2(player.transform.get_origin().x - transform.get_origin().x, player.transform.get_origin().y - transform.get_origin().y)
+		
 		direction = Vector2(clamp(distance_from_player.x, - 1, 1), clamp(distance_from_player.y, - 1, 1)).normalized()
-	
+		
+		print(int(direction.angle()))
+		match int(direction.angle()):
+			1:
+				direcao = "baixo"
+			-1:
+				direcao = "cima"
+			0:
+				direcao = "direita"
+			-3:
+				direcao = "esquerda"
 		if knockout.time_left == 0:
 			velocity.x =move_toward(velocity.x, direction.x * velocidade,velocidade/2)
 			velocity.y =move_toward(velocity.y, direction.y * velocidade,velocidade/2)
+			animated_sprite_2d.play("andando " + direcao)
 	else:
 		velocity.x = 0; velocity.y = 0
+		animated_sprite_2d.play("parado " + direcao)
 		
 
 	if knockout.time_left > 0:
 		if player:
 			velocity = -direction * 120
+	
 	move_and_slide()
 	
 
